8441111f53c217397582c224d37774c8c1be4206
Hirotoshi Uchida <uho02741358@gmail.com>
2026-03-04 17:29:22 +0900

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Generator from '@/components/Generator'
+import Documentation from '@/components/Documentation'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Generator />
+      <Documentation />
+    </main>
+  )
+}
